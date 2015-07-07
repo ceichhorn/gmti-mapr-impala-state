ea9f3f65a3112234de3a5879918b968124045906
Carl Eichhorn <ceichhorn@gmti.gannett.com>
2015-07-07 16:54:00 -0400

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['mapr']['impala_state'] = '10.54.101.1'
+default['mapr']['impala_catalog'] = '10.54.101.1'
+default['mapr']['impala_server'] = ['10.54.101.2', '10.54.101.3']
---
+++ spec/default_spec.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+require 'chefspec'
+require 'spec_helper'
+require 'chefspec/berkshelf'
+
+describe 'mapr_impala-state::default' do
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new do |node|
+    end.converge(described_recipe)
+  end
+
+  it 'installs package mapr-impala-catalog' do
+    expect(chef_run).to install_package('mapr-impala-catalog')
+  end
+  
+  it 'installs package mapr-impala-statestore' do
+    expect(chef_run).to install_package('mapr-impala-statestore')
+  end
+  
+  it 'run configure.sh -R' do
+  expect(chef_run).to run_bash('/opt/mapr/server/configure.sh -R')
+  end
+  
+   it 'chef should change env file' do
+  expect(chef_run).to run_ruby_block('Set Impala parameters')
+  end
+end
